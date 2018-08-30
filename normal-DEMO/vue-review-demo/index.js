0c6d26a5fc35e3b7c4b2c2fccf4f2825adbb9c1b
coolfe <coolfe36@gmail.com>
2018-08-30 09:29:40 +0800

    feat: add review vue demo

---
+++ normal-DEMO/vue-review-demo/index.js
@@ -0,0 +1,51 @@
+Vue.component("product-item", {
+    props: ["product"],
+    template: "<li>{{ product.name }}</li>"
+});
+
+var obj = {
+  freezeText: "foo"
+};
+Object.freeze(obj);
+
+
+var coolfe = new Vue({
+  el: "#app",
+  data: {
+    one: {
+      message: "Hello, Welcome to back.",
+      time: "load in " + new Date().toLocaleDateString(),
+      products: [],
+      reverseText: "Hello, Welcome to back.",
+      modelText: "Try to type some thins."
+    },
+    two: {
+      obj,
+      a: 1,
+    }
+  },
+  created() {
+    // 1.html
+    fetch("https://api.myjson.com/bins/74l63")
+      .then(response => {
+        console.log(response, "response");
+        return response.json();
+      })
+      .then(json => {
+        this.one.products = json.products;
+        console.log(json, "json");
+      });
+  },
+  methods: {
+    // 1.html
+    reverseText: function() {
+      this.one.reverseText = this.one.reverseText
+        .split("")
+        .reverse()
+        .join("");
+    }
+  },
+  created: function () {
+    console.log('a is: ' + this.two.a)
+  }
+});
